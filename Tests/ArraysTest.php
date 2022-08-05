4588284e10bf38bf5af901174bd0b0ee6df2c772
agrandesr <agrandesr@gmail.com>
2022-08-05 17:45:11 +0200

    Firsts steps with testing

--- Tests/ArraysTest.php
+++ Tests/ArraysTest.php
@@ -8,9 +8,68 @@ final class ArraysTest extends TestCase
 
     public function testHello(): void
     {
+        Arrays::setPathSeparator('.');
         $this->assertEquals(
             'hello',
             Arrays::hello()
         );
     }
+
+    public function testPathSetDeepChange() {
+        $firstTest=[
+            "1lvl1"=>[
+                "1lvl2"=>"Hello",
+                "1lvl2"=>"Test"
+            ],
+            "2lvl1"=>[
+                "1lvl2"=>"Hello",
+                "2lvl2"=>[
+                    "1lvl3"=>[
+                        "1lvl4"=>"There"
+                    ]
+                ]
+            ],
+            "Random string"
+        ];
+        $firstTry=Arrays::pathSet('1lvl1.1lvl2','Hi',$firstTest);
+
+        $this->assertEquals(
+            $firstTest,
+            $firstTry
+        );
+        $this->assertEquals(
+            $firstTry['1lvl1']['1lvl2'],
+            'Hi'
+        );
+
+    }
+
+    public function testPathSetRootChange(){
+        $array=[""=>["random"=>"random"]];
+        $firstTest=[""=>"test"];
+        $firstTry=Arrays::pathSet('',"test",$array);
+        $this->assertEquals(
+            $firstTest,
+            $firstTry
+        );
+
+    }
+
+    public function testPathSetSeparatorChange(){
+        Arrays::setPathSeparator('.');
+        $firstTest=["lvl1"=>["lvl2"=>["lvl3"=>'success']]];
+        $firstTry=Arrays::pathSet('lvl1.lvl2.lvl3','test',$firstTest,false);
+        $this->assertEquals(
+            'test',
+            $firstTry['lvl1']['lvl2']['lvl3']
+        );
+        Arrays::setPathSeparator('/');
+        $secondTest=$firstTry;
+        $secondTry=Arrays::pathSet('lvl1/lvl2/lvl3','2ndTest',$secondTest,false);
+        $this->assertEquals(
+            '2ndTest',
+            $secondTry['lvl1']['lvl2']['lvl3']
+        );
+    }
+    
 }
--- Utils/Arrays.php
+++ Utils/Arrays.php
@@ -1,7 +1,85 @@
 <?php
 namespace AGrandesR\Utils;
+
+use Error;
+
 class Arrays {
+    //region SEPARATOR methods
+    static function setPathSeparator(string $separator) : void {
+        $GLOBALS['X-AGRANDESR-ARRAYS-SEPARATOR']=$separator;
+    }
+
+    static function getPathSeparator() : string {
+        return $GLOBALS['X-AGRANDESR-ARRAYS-SEPARATOR'];
+    }
+    //endregion
+
     static function hello() {
         return "hello";
+    }
+    //FROM agile-router...
+    static function isArrayRouteSetInArray(string $arrayRoute, array $array, &$value=null) : bool {
+        $paths = explode('.',$arrayRoute);
+        $isSet=true;
+        foreach($paths as $path){
+            if(isset($array[$path])){
+                $value=$array[$path];
+                array_shift($paths);
+                if(!empty($paths)) return self::isArrayRouteSetInArray(implode('.',$paths),$array[$path]);
+            }else return false;
+        }
+        return $isSet;
+    }
+    
+    static function issetPath(string $path, $array) {
+
+    }
+
+    static function pathSet(string $path, mixed $value, array &$receivedArray, bool $byReference=true) : array {
+        self::avoidInfiniteLoop();
+
+        if($byReference) $workingArray = &$receivedArray;
+        else $workingArray = $receivedArray;
+
+        if(!isset($GLOBALS['X-AGRANDESR-ARRAYS-SEPARATOR'])) $GLOBALS['X-AGRANDESR-ARRAYS-SEPARATOR']='.';
+        $sprt=$GLOBALS['X-AGRANDESR-ARRAYS-SEPARATOR'];
+
+        $pathSlugs=explode($sprt,$path);
+
+        //We check if we are in the last slug of the path
+        $lastSlug=Count($pathSlugs)===1; //Remember, we remove one $slug in each call
+        $actualKeyOfTheArray=$pathSlugs[0]; //We collect the actual Slug - This is usefull to insert in the final iteration and to create the path to avoid problems with references
+        if($lastSlug) {
+            //We set the new value in the correct site
+            $workingArray[$actualKeyOfTheArray]=$value;
+            return  $workingArray;
+        }
+        unset($pathSlugs[0]); //We remove the 
+        $pathSlugs=array_values($pathSlugs); //Resort the array to avoid problems with the unset
+        if(!isset($workingArray[$actualKeyOfTheArray])) $workingArray[$actualKeyOfTheArray]=null;
+        self::pathSet(implode($sprt,$pathSlugs),$value,$workingArray[$actualKeyOfTheArray]);
+        return $workingArray;
+    }
+
+    static function pathGet(string $path, array $receivedArray) : mixed {
+        if(!isset($GLOBALS['X-AGRANDESR-ARRAYS-SEPARATOR'])) $GLOBALS['X-AGRANDESR-ARRAYS-SEPARATOR']='.';
+        $sprt=$GLOBALS['X-AGRANDESR-ARRAYS-SEPARATOR'];
+
+        $pathSlugs=explode($sprt,$path);
+
+        $tmpArray=$receivedArray;
+        foreach($pathSlugs as $segment) {
+            if(isset($tmpArray[$segment])){
+                $tmpArray=$tmpArray[$segment];
+            } else return null;
+        }
+        return $tmpArray;
+    }
+
+    static function avoidInfiniteLoop(string $msg='') : void {
+        if(isset($GLOBALS['X-AGRANDESR-AVOID-INFINITE'])) {
+            if($GLOBALS['X-AGRANDESR-AVOID-INFINITE-LOOP']>100) throw new Error("Infinite Loop: $msg", 1);
+            $GLOBALS['X-AGRANDESR-AVOID-INFINITE-LOOP']++;
+        }else $GLOBALS['X-AGRANDESR-AVOID-INFINITE-LOOP']=0;
     }
 }
